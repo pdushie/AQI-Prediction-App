5a6053dee6b56f4a2a6b2aef7f1e7606cf4373c1
Philip Dushie <66950563+pdushie@users.noreply.github.com>
2024-04-07 22:59:59 +0000

    Created prediction model, index.html, and flask api

---
+++ aqi_precit.py
@@ -0,0 +1,18 @@
+import pickle
+import bz2file as bz2
+
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, 'rb')
+    data = pickle.load(data)
+    return data
+
+model = decompress_pickle('/workspaces/AQI-Prediction-App/aqi_dt_comp.pbz2')
+
+def predict_aqi(no2_value, o3_value,so2_value,co_value):
+    
+    return (model.predict([[no2_value, o3_value, so2_value, co_value]]).item())
+
+print(predict_aqi(34.041667,0.0225 , 3, 1.145833))
+
+#print("Version: ",pickle.format_version)
+#print('The scikit-learn version is {}.'.format(sklearn.__version__))
