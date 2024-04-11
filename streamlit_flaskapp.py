700721e662a3600ffa1dc27223738ef9f1fc5f29
Philip Dushie <66950563+pdushie@users.noreply.github.com>
2024-04-11 15:01:32 -0300

    Update streamlit_flaskapp.py

--- streamlit_flaskapp.py
+++ streamlit_flaskapp.py
@@ -17,25 +17,3 @@ def predict_aqi(no2_value, o3_value,so2_value,co_value):
 
 
 
-app = Flask(__name__,template_folder='template')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/tryit/<int:var>',methods=['GET'])
-def tryit(var):
-    return({"Result":var})
-
-@app.route('/predict/', methods=['POST'])
-def predict():
-    data = request.get_json
-    x=data.get("x")
-    y=data.get("y")
-    z=data.get("z")
-    w=data.get("w")
-    result = predict_aqi(x,y,z,w)
-    return {'Result': result}
-
-if __name__ == '__main__':
-    app.run(debug=True)
