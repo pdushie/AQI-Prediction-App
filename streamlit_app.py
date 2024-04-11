4c8c6b78c4afdcd1523224047781712f939a3e98
Philip Dushie <66950563+pdushie@users.noreply.github.com>
2024-04-11 14:51:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ def predict_aqi(no2_value, o3_value,so2_value,co_value):
     
     return (model.predict([[no2_value, o3_value, so2_value, co_value]]).item())
 
-print(predict_aqi(34.041667,0.0225 , 3, 1.145833))
+#print(predict_aqi(34.041667,0.0225 , 3, 1.145833))
 
-print("Enter a number")
-input("Number:")
+#print("Enter a number")
+#input("Number:")
 
 #print("Version: ",pickle.format_version)
 #print('The scikit-learn version is {}.'.format(sklearn.__version__))
