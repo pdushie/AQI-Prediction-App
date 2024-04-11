2f108e3565c967bb7505ef1692892f4df7a1b527
Philip Dushie <66950563+pdushie@users.noreply.github.com>
2024-04-11 14:58:41 -0300

    Update streamlit_flaskapp.py

--- streamlit_flaskapp.py
+++ streamlit_flaskapp.py
@@ -1,5 +1,21 @@
 from  flask import Flask, request, jsonify,render_template
 #from streamlit_app.py import predict_aqi
+import pickle
+import bz2file as bz2
+import sklearn
+
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, 'rb')
+    data = pickle.load(data)
+    return data
+
+model = decompress_pickle('aqi_dt_comp.pbz2')
+
+def predict_aqi(no2_value, o3_value,so2_value,co_value):
+    
+    return (model.predict([[no2_value, o3_value, so2_value, co_value]]).item())
+
+
 
 app = Flask(__name__,template_folder='template')
 
